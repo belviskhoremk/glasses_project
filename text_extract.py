348364765522bf2adbdea5266e5a750210ac7efd
fitsum <97454009+fitsumgetachew@users.noreply.github.com>
2024-09-13 22:43:33 +0530

    added function to detect book and read book with voice

--- text_extract.py
+++ text_extract.py
@@ -1,6 +1,36 @@
+import cv2
 from PIL import Image
-from pytesseract import pytesseract
+# from pytesseract import pytesseract
 
+
+def preprocess_image(image):
+    # Convert to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Apply Gaussian blur to reduce noise
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+
+    # Apply thresholding
+    _, thresh = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    return thresh
+
+from paddleocr import PaddleOCR, draw_ocr
+
+def pdp(img_path):
+    ocr = PaddleOCR(use_angle_cls=True, lang='en')  # need to run only once to download and load model into memory
+
+
+    # Extract text
+    result = ocr.ocr(img_path, cls=True)
+    extracted_text = []
+    for line in result:
+        for word_info in line:
+            text, confidence = word_info[1]
+            extracted_text.append((text, confidence))
+
+    for text, confidence in extracted_text:
+        print(f"Text: {text}, Confidence: {confidence}")
 
 def extract_text(path , lang ="eng"):
     """
@@ -9,15 +39,20 @@ def extract_text(path , lang ="eng"):
     :param lang:preferernce language
     :return -> str:extracted text form a given image
     """
-    img  = Image.open(path)
+    pass
+    image = cv2.imread(path)
 
-    text = pytesseract.image_to_string(img, lang=lang)
+    # Convert the processed image to PIL format
+    img_pil = Image.fromarray(image)
+
+    # Extract text using Tessersact OCR
+    text = pytesseract.image_to_string(img_pil, lang=lang)
 
     print(text)
     print("\n\ntype", type(text))
 
     return text
 
-# if __name__ == '__main__':
-#     path = 'sample-images/sample_book2.jpg'
-#     extract_text(path)
+if __name__ == '__main__':
+    path = 'sample-images/sample_book4.jpg'
+    pdp(path)
